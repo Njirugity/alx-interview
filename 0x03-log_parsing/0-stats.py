b0fc114a401ba05a4f0f51fa22fe07e84dd4e16b
brian <muriithibrian283@gmail.com>
2025-05-08 19:21:52 +0300

    Add solution to log parsing task

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""A script that reads stdin line by line and computes metrics"""
+import sys
+import re
+
+
+total = 0
+status_count = {}
+line_count = 0
+
+
+def print_log():
+    """
+     Return:
+            -Total file size
+            -Status code and its count
+    """
+
+    print(f'File size: {total}')
+    for code in sorted(status_count.keys()):
+        print(f"{code}: {status_count[code]}")
+
+
+try:
+    for line in sys.stdin:
+        match = re.search(r'"[^"]+" (\d{3}) (\d+)', line)
+        if match:
+            code = match.group(1)
+            file_size = match.group(2)
+            total += int(file_size)
+            if code in status_count:
+                status_count[code] += 1
+            else:
+                status_count[code] = 1
+
+        line_count += 1
+        if line_count % 10 == 0:
+            print_log()
+
+except KeyboardInterrupt:
+    print_log()
+    raise
+
+print_log()
